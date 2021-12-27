79551d35082cadd6338c6cb9ad859cbb25418e25
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2021-12-27 11:47:34 +0100

    Create update.sh
    Inital commit

---
+++ scripts/update.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#######################FUNCTIONS###############################################################
+function pause(){
+ read -s -n 1 -p "Press any key to continue . . ."
+ echo ""
+}
+######################FUNCTIONS#################################################################
+
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED='\e[1;31m'
+TEXT_BLUE='\e[1;34m'
+
+###################################SET TIMEZONE#################################################
+sudo timedatectl set-timezone Europe/Brussels
+echo -e $TEXT_YELLOW
+echo 'Timezone changed to Europe/Brussels'
+echo -e $TEXT_RESET
+
+
+###################################UPDATE STARTED################################################
+sudo apt-get update
+echo -e $TEXT_YELLOW
+echo 'APT update finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get dist-upgrade -y
+echo -e $TEXT_YELLOW
+echo 'APT distributive upgrade finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get upgrade -y
+echo -e $TEXT_YELLOW
+echo 'APT upgrade finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get autoremove -y
+echo -e $TEXT_YELLOW
+echo 'APT auto remove finished...'
+echo -e $TEXT_RESET
+clear
+echo -e $TEXT_BLUE
+echo 'Server is updated... Moving on'
+echo -e $TEXT_RESETecho -e $TEXT_YELLOW
+echo 'Adding new administrator: Please input username.'
+echo -e $TEXT_RESET
+read -p 'Username:' username
+echo -e $TEXT_YELLOW
+echo 'Administrator created. Please type in password:'
+echo -e $TEXT_RESET
+read -sp 'Password:' password
+sudo useradd -m -p $password -s /bin/bash $username
+echo
+clear
+echo -e $TEXT_BLUE
+echo Thank you $username, we know have your login details.
+echo -e $TEXT_RESET
+usermod -aG sudo $username
+pause
+##################ADMINISTRATOR CREATED############################################################
+
+###############CREATE FIVEM USER###############################################################
+echo -e $TEXT_YELLOW
+echo 'We are gonna create an user without privileges: fivem'
+sudo useradd -m -p fivem -s /bin/bash fivem
+echo 'User fivem is created. Password is also fivem'
+echo -e $TEXT_RESET
+pause
+clear
+##################FIVEM USER CREATED############################################################
+
+#######################INSTALL FIVEM#####################################################
+sudo -u fivem installfivem.sh
+pause
+#######################FIVEM INSTALLED####################################################
+
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo 'Reboot required!'
+    echo -e $TEXT_RESETa
+    sudo reboot now
+fi
+
+pause
+###################################UPDATE FINISHED################################################
+
+###############CREATE ADMINISTRATOR###############################################################
+clear
+echo -e $TEXT_RED
+echo 'Create server administrator'
+echo -e $TEXT_RESET
